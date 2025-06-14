bbaf43411e07773c8dce7d7c52da5f458ce0cc41
ARUQUIPA FLORES ALAN NAHUEL <aaruquipaflores639@alumno.unlam.edu.ar>
2025-06-14 00:12:29 -0300

    config implemented

---
+++ src/main/java/com/ar/javalin/base/App.java
@@ -0,0 +1,57 @@
+package com.ar.javalin.base;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.json.JavalinJackson;
+import io.javalin.json.JsonMapper;
+import com.truncon.javalin.mvc.ControllerRegistry;
+import com.truncon.javalin.mvc.JavalinControllerRegistry;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.openapi.OpenApiInfo;
+import io.javalin.openapi.plugin.OpenApiConfiguration;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
+import lombok.extern.slf4j.Slf4j;
+import io.javalin.openapi.plugin.swagger.SwaggerConfiguration;
+
+@Slf4j
+public final class App {
+    public static void main(String[] args) throws IOException {
+        Javalin app = Javalin.create(config -> {
+            // Remove the following line to disable Open API annotation processing
+            config.plugins.register(new OpenApiPlugin(getOpenApiOptions()));
+            config.plugins.register(new SwaggerPlugin(new SwaggerConfiguration()));
+            
+            // Register you JSON mapper with whatever library you want
+            ObjectMapper objectMapper = new ObjectMapper(); // customize as needed
+            JsonMapper jsonMapper = new JavalinJackson(objectMapper);
+            config.jsonMapper(jsonMapper);
+            
+            // This example is using the SPA feature with static files
+          //  config.staticFiles.add("./public", Location.EXTERNAL);
+          //  config.staticFiles.add("/", Location.EXTERNAL);
+        });
+
+        // Javalin MVC generates "com.truncon.javalin.mvc.JavalinControllerRegistry" automatically at compile time
+        ControllerRegistry registry = new JavalinControllerRegistry();
+        registry.register(app);
+
+        // Prevent unhandled exceptions from taking down the web server
+        app.exception(Exception.class, (e, ctx) -> {
+            log.error("Encountered an unhandled exception.", e);
+            ctx.status(500);
+        });
+
+        app.start(8080);
+    }
+
+    private static OpenApiConfiguration getOpenApiOptions() {
+        OpenApiConfiguration configuration = new OpenApiConfiguration();
+        OpenApiInfo info = configuration.getInfo();
+        info.setTitle("My API");
+        info.setVersion("1.0");
+        return configuration;
+    }
+}
