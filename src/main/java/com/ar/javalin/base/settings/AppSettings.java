93620724e59ade3822bcd9a2d95f721e1d33e908
ARUQUIPA FLORES ALAN NAHUEL <aaruquipaflores639@alumno.unlam.edu.ar>
2025-07-07 23:02:33 -0300

    fix

--- src/main/java/com/ar/javalin/base/settings/AppSettings.java
+++ src/main/java/com/ar/javalin/base/settings/AppSettings.java
@@ -2,11 +2,11 @@ package com.ar.javalin.base.settings;
 
 public class AppSettings implements ApplicationSettings{
     private final int PORT = 8080;
-
-
     
     @Override
     public int getPort() {
         return PORT;
     }
+
+
 }
